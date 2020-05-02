506b7a8a84ac534417b2f53d489d8ff062d98ef7
parammittal162 <param.mittal@chqbook.com>
2020-05-02 21:39:22 +0530

    initial Commit

---
+++ app.js
@@ -0,0 +1,17 @@
+/*eslint-env es6*/
+const express = require('express');
+const bodyParser = require('body-parser');
+// const { PythonShell } = require('python-shell');
+// const multer = require('multer');
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => res.send('Mini Hospital API 2'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+// python3 predict.py '/home/ubuntu18/Documents/My/00 PROJECTS/4rth YEAR PROJECT/mini-hospital-api/python-scripts/bone-fracture-detection/bone.png'
