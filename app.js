cc30288a4770345821982f2cb1078448dbe480e9
parammittal162 <param.mittal@chqbook.com>
2020-05-02 22:16:20 +0530

    initial Commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /*eslint-env es6*/
 const express = require('express');
 const bodyParser = require('body-parser');
-// const { PythonShell } = require('python-shell');
+const { PythonShell } = require('python-shell');
 // const multer = require('multer');
 
 
@@ -9,6 +9,24 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+
+app.get('/sample', (req, res) => {
+    var options = {
+        args:
+            [
+                req.query.funds, // starting funds
+                req.query.size, // (initial) wager size
+                req.query.count, // wager count — number of wagers per sim
+                req.query.sims // number of simulations
+            ]
+    }
+    console.log("------>", options);
+    PythonShell.run('./python-scripts/sample.py', options, function (err, data) {
+        if (err) res.send(err);
+        console.log(data.toString());
+        res.send(data.toString())
+    });
+});
 
 app.get('/', (req, res) => res.send('Mini Hospital API 2'));
 
